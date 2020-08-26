65cdd2e887293243ad7801b7095404e8478c8976
silliest <68700643+silliests@users.noreply.github.com>
2020-08-26 16:26:49 -0700

    Some beginning files

---
+++ main.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('Donut Bot is online!')
+})
+
+
+
+
+client.login('NzQ4MzA2NTU5NDYwOTY2NDMy.X0bgyg.AbKYUgLQM9UuGK9j-3tcB61fAeM');
