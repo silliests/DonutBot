1f2c988c56bbe75039a8ba6376e8741283016183
silliest <68700643+silliests@users.noreply.github.com>
2020-08-26 18:28:36 -0700

    test commands
    added test commands
    
    !ping --> outputs "pong!"
    
    findseed --> simulates random chance of eyes in end portal

--- main.js
+++ main.js
@@ -9,16 +9,30 @@ require('dotenv').config();
 const key = process.env.key;
 const prefix = '!';
 
-client.on('message', message =>{
-    if(!message.content.startsWith(prefix) || !message.author.bot) return;
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+  }
 
-   
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
 
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
     if(command === 'ping'){
-        message.channel.send('pong');
+        message.channel.send('pong!');
+    }
+    if (command === 'findseed'){
+    var i = 0;
+    var eyes = 0;
+    for(i = 0; i < 12; i++) {
+        if (getRandomInt(0, 10) === 1) {
+            eyes++;
+        }
+    }
+    message.channel.send(message.author.username + ' --> Your seed is a ' + eyes + ' eye.')
     }
 })
 
