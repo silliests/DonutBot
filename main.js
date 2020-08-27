5c8249108fb8388ac51eb7eeb714c924d4c02485
silliest <68700643+silliests@users.noreply.github.com>
2020-08-26 17:09:21 -0700

    Key-Hiding testing
    pog champ

--- main.js
+++ main.js
@@ -5,8 +5,9 @@ const client = new Discord.Client();
 client.once('ready', () => {
     console.log('Donut Bot is online!')
 })
-
-
+require('dotenv').config();
+const key = process.env.key;
+console.log(key);
 
 
 client.login('NzQ4MzA2NTU5NDYwOTY2NDMy.X0bgyg.AbKYUgLQM9UuGK9j-3tcB61fAeM');
